1626e50310f0282d39a98c792c245d45e734851f
ashish <ashishprasad949@gmail.com>
2023-12-03 18:16:28 +0530

    connect database

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import mongoose, { connect } from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const dataIns = await mongoose.connect(
+      `${process.env.MONGO_URI}${DB_NAME}`,
+    );
+    console.log(`DataBase is Connected 🎉 ${dataIns}`);
+  } catch (error) {
+    console.log(`MongoBD Error 🫡 : ${error}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
